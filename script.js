82b4078c839c96d91c00b15973d249b3586bf7e5
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-12 01:23:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-window.addEventListener('resize', function() {
-    if (window.innerWidth < 1042) {
-      sidebar.classList.add("active");
-    }
-    else sidebar.classList.remove("active");
-
+document.addEventListener("DOMContentLoaded", function() {
+    // const sidebarCollapse = document.getElementById("sidebarCollapse");
+    const sidebar = document.getElementById("sidebar");
+    const mainContent=document.querySelector(".main")
+    
+  
+    // sidebarCollapse.addEventListener("click", function() {
+    //   sidebar.classList.toggle("active");
+    // });
   });
+  
+
+ 
