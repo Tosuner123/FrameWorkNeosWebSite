c266e4402113f1a5228fdf78738dff4434c74b81
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-12 12:50:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-  window.addEventListener('resize', function() {
-    if (window.innerWidth < 1042) {
-      sidebar.classList.add("active");
-    }
-    else sidebar.classList.remove("active");
 
-  });
