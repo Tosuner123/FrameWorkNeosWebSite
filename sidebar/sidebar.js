b71fd19243cf3868875eec437de0e0c86398f573
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-18 14:42:17 +0300

    Update sidebar.js

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,21 +1,21 @@
-const head = document.getElementsByTagName('head')[0];
-const script1 = document.createElement('script');
-const script2 = document.createElement('script');
-const script3 = document.createElement('script');
-script1.src = "https://use.fontawesome.com/releases/v5.0.13/js/solid.js";
-script1.integrity = "sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ";
-script1.crossOrigin = "anonymous";
-script1.defer = true;
-script2.src = "https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js";
-script2.integrity = "sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY";
-script2.crossOrigin = "anonymous";
-script2.defer = true;
-script3.src = "https://kit.fontawesome.com/8c025b47e7.js";
-script3.crossOrigin = "anonymous";
-script3.defer = true;
-head.appendChild(script1);
-head.appendChild(script2);
-head.appendChild(script3);
+// const head = document.getElementsByTagName('head')[0];
+// const script1 = document.createElement('script');
+// const script2 = document.createElement('script');
+// const script3 = document.createElement('script');
+// script1.src = "https://use.fontawesome.com/releases/v5.0.13/js/solid.js";
+// script1.integrity = "sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ";
+// script1.crossOrigin = "anonymous";
+// script1.defer = true;
+// script2.src = "https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js";
+// script2.integrity = "sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY";
+// script2.crossOrigin = "anonymous";
+// script2.defer = true;
+// script3.src = "https://kit.fontawesome.com/8c025b47e7.js";
+// script3.crossOrigin = "anonymous";
+// script3.defer = true;
+// head.appendChild(script1);
+// head.appendChild(script2);
+// head.appendChild(script3);
 
 
 
