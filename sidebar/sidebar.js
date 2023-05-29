c03a75883fbb01eaef1867320922bbb7d5882708
BURAK <131662168+Tosuner123@users.noreply.github.com>
2023-05-29 16:39:55 +0300

    Update sidebar.js

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -53,6 +53,28 @@ kopyala.forEach(btn =>{
     window.scrollTo(0, scrollY) // sayfanın kaydedilen scroll pozisyonuna geri dön
   }); 
 })
+
+                // LOADER
+
+   
+   document.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.tagName === 'A' && target.getAttribute('href') !== '#') {
+      event.preventDefault();
+      const loaderContainer = document.createElement('div');
+      loaderContainer.classList.add('loader-container');
+      loaderContainer.innerHTML = `
+        <div class="spinner">
+          <div class="n-harfi">N</div>
+        </div>
+      `;
+      document.body.appendChild(loaderContainer);
+
+      setTimeout(() => {
+        window.location.href = target.href;
+      }, 1000);
+    }
+  });
   
 
 
